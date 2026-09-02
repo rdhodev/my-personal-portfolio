ae7b45825efd85304112d05042503579eea0b7e0
Ridho Dev <rdho.hdyat@gmail.com>
2026-09-02 13:50:42 +0700

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,19 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  description: "Portfolio ",
   title: "Ridho Hidayat - Personal Portfolio",
+  description: "Portfolio ",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon.ico" },
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/favicon/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
